10402d16c231292906ef4b28dc65cb01d01c29fa
Dekkee <dekker25@gmail.com>
2017-03-18 14:09:36 +0300

    Докоммит проектного файла Настройки appveyor

--- src/SoSemiVigelant.FrontEnd/server.js
+++ src/SoSemiVigelant.FrontEnd/server.js
@@ -7,9 +7,7 @@ const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function(req, res) {
-    res.send('Hello world\n');
-});
+app.use(express.static('public'))
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
