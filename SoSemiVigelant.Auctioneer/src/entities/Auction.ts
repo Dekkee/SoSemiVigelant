fa53a38139a8c77e4a2813a6d2faaf961c267b41
Dekker <dekker25@gmail.com>
2018-03-03 17:47:48 +0300

    auctioneer: Auction model

--- SoSemiVigelant.Auctioneer/src/entities/Auction.ts
+++ SoSemiVigelant.Auctioneer/src/entities/Auction.ts
@@ -2,10 +2,44 @@ import { Schema, Document, Model, model } from 'mongoose';
 
 export interface IAuctionModel extends Document {
     name: string;
+    lot: string;
+    date_estimated: Date;
+    image: string;
+    start_bid: number;
+    current_bid: number;
+    bid_amount: number;
+    shipping_info_quick: string;
+    shipping_info: string;
+    bin_value: number;
+    date_published: Date;
+    thumb_is_generic: boolean;
+    with_post: boolean;
+    city: string;
+    seller_ip: string;
+    seller: string;
 }
 
 export var AuctionSchema: Schema = new Schema({
-    name: String,
-});
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    lot: String,
+    date_estimated: Date,
+    image: String,
+    start_bid: Number,
+    current_bid: Number,
+    bid_amount: Number,
+    shipping_info_quick: String,
+    shipping_info: String,
+    bin_value: Number,
+    date_published: Date,
+    thumb_is_generic: Boolean,
+    with_post: Boolean,
+    city: String,
+    seller_ip: String,
+    seller: String
+}, { id: false });
 
-export const Auction: Model<IAuctionModel> = model<IAuctionModel>("auctions", AuctionSchema);
+export const Auction: Model<IAuctionModel> = model<IAuctionModel>('auctions', AuctionSchema);
