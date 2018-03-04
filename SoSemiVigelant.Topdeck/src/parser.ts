a9045491a4f20231ee2054042013409cb0c44bcb
Dekker <dekker25@gmail.com>
2018-03-04 15:12:52 +0300

    fix(topdeck): https fix

--- SoSemiVigelant.Topdeck/src/parser.ts
+++ SoSemiVigelant.Topdeck/src/parser.ts
@@ -2,7 +2,7 @@ const requirejs = require('requirejs'),
     jsdom = { ...require('jsdom') },
     jquery = require('jquery');
 
-import * as request from 'request';
+import { request } from 'https';
 import * as cheerio from 'cheerio';
 import * as colors from 'colors';
 
@@ -14,7 +14,7 @@ const localStorage = {
 
 (global as any).localStorage = localStorage;
 
-const url = 'http://topdeck.ru/apps/toptrade/auctions';
+const url = 'https://topdeck.ru/apps/toptrade/auctions';
 
 export const parse = (): Promise<{}> => {
     return new Promise((resolve, reject) => {
@@ -43,29 +43,37 @@ export const parse = (): Promise<{}> => {
                 define: requirejs.define
             };
 
-            request({ uri: url, method: 'GET', encoding: 'binary' },
-                function (err, res, page) {
-                    if (err) {
-                        console.error(colors.red('Error requestinf data: ') + err);
-                        return;
-                    }
-                    const cheer = cheerio.load(page);
-                    const scripts = cheer('script');
-                    for (let index in scripts) {
-                        if (scripts[index].children &&
-                            scripts[index].children.length &&
-                            scripts[index].children[0] &&
-                            // todo: чёт хардкод
-                            scripts[index].children[0].data!.indexOf('topdeck.require([\'jquery\', \'knockout\'],') !== -1) {
-                            try {
-                                eval(scripts[index].children[0].data!);
-                            } catch (e) {
-
+            const req = request({
+                    hostname: 'topdeck.ru',
+                    path: '/apps/toptrade/auctions',
+                    method: 'GET',
+                    ecdhCurve: 'secp384r1'
+                },
+                (res: any) => {
+                    const data: any[] = [];
+                    res.on('data', (chunk: any) => data.push(chunk));
+                    res.on('end', () => {
+                        const page = Buffer.concat(data);;
+                        const cheer = cheerio.load(page.toString());
+                        const scripts = cheer('script');
+                        for (let index in scripts) {
+                            if (scripts[index].children &&
+                                scripts[index].children.length &&
+                                scripts[index].children[0] &&
+                                scripts[index].children[0].data &&
+                                // todo: чёт хардкод
+                                scripts[index].children[0].data!.indexOf('topdeck.require([\'jquery\', \'knockout\'],') !== -1) {
+                                try {
+                                    eval(scripts[index].children[0].data!);
+                                } catch (e) {
+                                }
                             }
                         }
-                    }
+                    });
                 }
             );
+            req.on('error', e => console.error(e));
+            req.end();
         });
     });
 };
