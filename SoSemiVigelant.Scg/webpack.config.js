354cadebcc45e4afd2f7324242689cfd0ca8d29a
Dekker <dekker25@gmail.com>
2018-07-30 08:49:42 +0300

    fix(scg): fix dev build

--- SoSemiVigelant.Scg/webpack.config.js
+++ SoSemiVigelant.Scg/webpack.config.js
@@ -1,39 +1,48 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
-module.exports = (env) => ({
-    entry: './src/index.tsx',
-    mode: env || 'development',
-    module:{
-        rules: [
-            { test: /\.tsx?/, use: 'awesome-typescript-loader' },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader",   // translates CSS into CommonJS
-                    "sass-loader"   // compiles Sass to CSS
-                ]
-            },
-        ]
-    },
-    plugins: [
+
+module.exports = (env) => {
+    const plugins = [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: path.join('./src/template/index.html'),
         }),
-        env && new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(env)
         })
-    ],
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx']
-    },
-    devServer: {
-        compress: true,
-        contentBase: './dist',
-        hot: true
-    },
-    devtool: env && 'cheap-source-map',
-});
+    ];
+    if (env !== 'production') {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    return {
+        entry: './src/index.tsx',
+        mode: env || 'development',
+        module: {
+            rules: [{
+                    test: /\.tsx?/,
+                    use: 'awesome-typescript-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        "style-loader", // creates style nodes from JS strings
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS
+                    ]
+                },
+            ]
+        },
+        plugins,
+        resolve: {
+            extensions: ['.js', '.ts', '.tsx']
+        },
+        devServer: {
+            compress: true,
+            contentBase: './dist',
+            hot: true
+        },
+        devtool: env && 'cheap-source-map',
+    }
+};
