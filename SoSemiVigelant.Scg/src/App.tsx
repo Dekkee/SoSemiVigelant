1fb98dd19bbaad344956bed0830b30c479c8139a
Dekker <dekker25@gmail.com>
2018-07-30 21:08:16 +0300

    fix(scg): fix result from previous requests fix empty string requests fix viewport

--- SoSemiVigelant.Scg/src/App.tsx
+++ SoSemiVigelant.Scg/src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as cn from 'classnames';
+import AbortController from 'abort-controller';
+
 import { ChangeEvent } from "react";
 import { debounce } from 'lodash';
 import { ParsedRow } from "./entities/Row";
-import { CardLayout } from "./components/CardLayout/CardLayout";
+import { CardLayout } from "./components/CardLayout";
 
 import './App.scss';
 
@@ -16,25 +18,38 @@ const url = process.env.NODE_ENV === 'production'
     : '//localhost:8081';
 
 export class App extends React.Component<{}, IState> {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.state = {
             rows: []
         }
+
+        this.controller = new AbortController();
     }
 
-    requestData = debounce(async (value: string) => this.setState({ rows: await (await fetch(`${url}/api?name=${value}`)).json() as ParsedRow[] }), 300);
+    private controller: AbortController;
+
+    requestData = debounce(async (value: string) => {
+        if (!value) {
+            return;
+        }
+        this.controller.abort();
+        const rows = await (await fetch(`${url}/api?name=${value}`, { signal: this.controller.signal })).json() as ParsedRow[];
+        if (rows) {
+            this.setState({ rows });
+        }
+    }, 300);
 
     onInput = (e: ChangeEvent) => this.requestData((e.target as HTMLInputElement).value);
 
-    render () {
+    render() {
         const { rows } = this.state;
         return (
             <>
                 <label>
                     Search:
-                    <input onChange={ (e) => this.onInput(e) }/>
+                    <input onChange={(e) => this.onInput(e)} />
                 </label>
                 {
                     rows.length
@@ -46,9 +61,9 @@ export class App extends React.Component<{}, IState> {
                         //     <span>Condition</span>
                         //     <span>Price</span>
                         //    {
-                                rows.map((row, i) => <CardLayout className={ cn({ "card-layout--dark": i % 2 }) }
-                                                                 card={ row }
-                                                                 key={ i }/>)
+                        rows.map((row, i) => <CardLayout className={cn({ "card-layout--dark": i % 2 })}
+                            card={row}
+                            key={i} />)
                         //    }
                         // </div>
                         : <div>empty</div>
