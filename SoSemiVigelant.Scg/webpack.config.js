27ff76fb865117cd211c3aceb096e5ae507d0fc2
Dekker <dekker25@gmail.com>
2018-08-04 20:41:21 +0300

    pwa(scg): ios true

--- SoSemiVigelant.Scg/webpack.config.js
+++ SoSemiVigelant.Scg/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = (env) => {
             background_color: "#FFFFFF",
             fingerprints: false,
             inject: true,
+            ios: true,
             "icons": [
                 {
                     src: path.resolve(__dirname, './src/icons/android-icon-192x192.png'),
